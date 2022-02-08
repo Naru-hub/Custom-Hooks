9a349be08ff003229c85d421043957709b952b38
Nona <81672054+Naru-hub@users.noreply.github.com>
2022-02-08 22:30:57 +0900

    カスタムフックを使ってデータ取得 カスタムフックを使わないデータ取得→カスタムフックを使ってデータ取得に変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [UserProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
+  const onClickFetchUser = () => getUsers();
 
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
@@ -41,7 +17,7 @@ export default function App() {
         <p>Loading...</p>
       ) : (
         <>
-          {UserProfiles.map((user) => (
+          {userProfiles.map((user) => (
             <UserCard key={user.id} user={user} />
           ))}
         </>
